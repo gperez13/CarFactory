c4ed1dc8cf8d6a8fd68f9f7736064ea2ca7f00c3
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2018-01-25 15:09:23 -0600

    Created car specs function and an assembly line function to continue manufacturing cars or to halt production. This assemblyLine function also has a test for imputs to account for upper and lower case characters

--- src/Car.java
+++ src/Car.java
@@ -16,9 +16,45 @@ public class Car {
         }
     }
 
+    public int getDoors() {
+        return doors;
+    }
 
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     public String getModel(){
         return this.model;
     }
+
+
+
 }
+
+
+// Remember getters and setters
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,75 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String [] args){
+
+        specs();
+
+
         Car audi = new Car();
         Car porsche = new Car();
         audi.setModel("TT");
 
         System.out.println("Model is " + audi.getModel());
+
+
+    }
+
+
+
+    public static void specs(){
+
+        String name;
+        int carDoors;
+        int carWheels;
+        String carModel;
+        String carEngine;
+        String carColor;
+
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("What is the name of the car manufacturer?");
+        name = scan.next();
+        System.out.println("How many doors does it have?");
+        carDoors = scan.nextInt();
+        System.out.println("How many wheels does it have?");
+        carWheels = scan.nextInt();
+        System.out.println("what is the model?");
+        carModel = scan.next();
+        System.out.println("What type of engine does it have?");
+        carEngine = scan.next();
+        System.out.println("What is the car color?");
+        carColor = scan.next();
+
+
+        System.out.println(name + " " + carDoors + " " + carWheels + " " + carModel + " " + carEngine + " " + carColor);
+
+
+        assemblyLine();
+
+    }
+
+
+
+
+    public static void assemblyLine(){
+
+        String directions;
+        String directive;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Are we making another car? (yes/no");
+        directions = scan.next();
+        directive = directions.toLowerCase();
+
+        if(directive.equals("y") || directive.equals("yes")){
+            specs();
+
+        } else {
+            System.out.println("The assembly line is finished!");
+        }
 
 
     }
