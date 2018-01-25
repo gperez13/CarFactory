0f773390621d456d5e44f0daf6acc6eaa04a2d57
Giovanni Perez <giovanniperez@Giovannis-MacBook-Pro.local>
2018-01-25 11:34:59 -0600

    created Car object properties and instantiated object

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    public static void main(String [] args){
+        Car audi = new Car();
+        Car porsche = new Car();
+        audi.model = "TT";
+
+        System.out.println(audi.model);
+
+
+    }
+
 }
